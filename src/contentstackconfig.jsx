63da941f65920c8cef45447a6ae87242d6f9aebd
Jithu <129660199+JithuMorrison@users.noreply.github.com>
2024-10-21 20:34:16 +0530

    Update contentstackconfig.jsx

--- src/contentstackconfig.jsx
+++ src/contentstackconfig.jsx
@@ -2,10 +2,10 @@
 import { Stack } from 'contentstack';
 
 const stack = Stack({
-  api_key: 'bltab5f33fe9473d688',
-  delivery_token: 'cs909f260cdf07dda4e4e4dac5',
-  environment: 'development',
-  region: 'EU' 
+  api_key: 'API_KEY',
+  delivery_token: 'DELIVERY_TOKEN',
+  environment: 'ENV',
+  region: 'REGION' 
 });
 
 export default stack;
