e460d3e5ef01c03f9b8e70e597a34044782df155
JithuMorrison <jithus2004@gmail.com>
2025-07-15 20:50:57 +0530

    modified UI

--- src/templatebar.jsx
+++ src/templatebar.jsx
@@ -103,7 +103,7 @@ const styles = {
     backgroundColor: '#f9f9f9',
     borderRadius: '12px',
     boxShadow: '0 0 10px rgba(0,0,0,0.1)',
-    width: '350px',
+    width: '300px',
     overflowY: 'auto',
   },
   heading: {
